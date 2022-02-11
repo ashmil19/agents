0be09bdb0f17436b3d6e7ae63a7b3c4d6ca72f2f
ashmil19 <ashmilabdulrasheed@gmail.com>
2022-02-11 14:13:53 +0530

    download_middelewares added to settings.py

--- agents/settings.py
+++ agents/settings.py
@@ -48,6 +48,11 @@ ROBOTSTXT_OBEY = True
 #    'agents.middlewares.AgentsSpiderMiddleware': 543,
 #}
 
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+}
+
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
