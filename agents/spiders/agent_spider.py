2ccf6781ce9dee198473c25af3fa1c025ba5bdce
ashmil19 <ashmilabdulrasheed@gmail.com>
2022-02-11 11:39:46 +0530

    profile link added and create forloop for get each profiles

--- agents/spiders/agent_spider.py
+++ agents/spiders/agent_spider.py
@@ -3,7 +3,13 @@ import scrapy
 
 class AgentSpiderSpider(scrapy.Spider):
     name = 'agents'
-    start_urls = ['http://https://www.bhhsamb.com/agents/']
+    start_urls = [
+        'http://https://www.bhhsamb.com/agents/'
+        ]
 
     def parse(self, response):
-        pass
+        
+        profile_link = response.xpath("//span[@class='agent-name']/a/@href").extract()
+        
+        for link in profile_link:
+            yield response.follow(link, callback=self.parse_details)
