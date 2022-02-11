3f5d4158c9c2fb7d2f203e8a7f9e3b9ea8d20720
ashmil19 <ashmilabdulrasheed@gmail.com>
2022-02-11 14:04:55 +0530

    items fields is added

--- agents/items.py
+++ agents/items.py
@@ -9,4 +9,13 @@ import scrapy
 class AgentsItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    
+    name = scrapy.Field()
+    job_title = scrapy.Field()
+    img_url = scrapy.Field()
+    address = scrapy.Field()
+    contact_details = scrapy.Field()
+    social_accounts = scrapy.Field()
+    offices = scrapy.Field()
+    languages = scrapy.Field()
+    description = scrapy.Field()
